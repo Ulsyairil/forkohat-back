30aa4757fabf4ca85b899b6137a7786bdf23ee45
Ulsyairil <ulsyairil@outlook.co.id>
2023-10-12 11:50:49 +0800

    Fix ecosystem for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,17 @@ module.exports = {
   apps: [
     {
       name: 'forkohat-back',
-      script: './server.js',
-      instances: 'max',
       exec_mode: 'cluster',
+      script: './server.js',
+      instances: '1',
+      wait_ready: true,
       autorestart: true,
+      max_restarts: 1,
+      watch: true,
+      error_file: 'logs/forkohat-back/err.log',
+      out_file: 'logs/forkohat-back/out.log',
+      log_file: 'logs/forkohat-back/combined.log',
+      time: true,
     },
   ],
 }
