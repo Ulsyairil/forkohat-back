668f305d5bdaaaab79df2360ab9251e1d29cf404
Ulsyairil <ulsyairil@outlook.co.id>
2024-08-25 21:10:07 +0800

    fix pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'forkohat-back',
+      name: 'forkohat-app',
       exec_mode: 'cluster',
       script: './server.js',
       instances: '1',
       wait_ready: true,
       autorestart: true,
       max_restarts: 1,
-      watch: true,
-      error_file: 'logs/forkohat-back/err.log',
-      out_file: 'logs/forkohat-back/out.log',
-      log_file: 'logs/forkohat-back/combined.log',
+      watch: false,
       time: true,
+      error_file: '.logs/err.log',
+      out_file: '.logs/out.log',
+      merge_logs: false,
+      log_type: "json",
     },
   ],
 }
