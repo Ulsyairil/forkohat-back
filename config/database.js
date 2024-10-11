12f59eea6583448ca6961f04e769011608e17fd4
Ulsyairil <ulsyairil@outlook.co.id>
2024-10-11 21:51:14 +0800

    add mysql2

--- config/database.js
+++ config/database.js
@@ -45,12 +45,35 @@ module.exports = {
   |--------------------------------------------------------------------------
   |
   | Here we define connection settings for MySQL database.
+  | Use it only version < 8
   |
   | npm i --save mysql
   |
   */
   mysql: {
     client: 'mysql',
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis'),
+    },
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | MySQL2
+  |--------------------------------------------------------------------------
+  |
+  | Here we define connection settings for MySQL2 database.
+  | Use it only version >= 8
+  |
+  | npm i --save mysql2
+  |
+  */
+  mysql2: {
+    client: 'mysql2',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
